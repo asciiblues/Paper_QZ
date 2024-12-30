07b8c5cf62f84452e3eca640103cacd0240dfcdf
asciiblues <nzzz101.3z@gmail.com>
2024-12-30 19:40:13 +0530

    main

--- andapp/app/src/main/java/com/asciiblues/paperqz/info.java
+++ andapp/app/src/main/java/com/asciiblues/paperqz/info.java
@@ -1,8 +1,11 @@
 package com.asciiblues.paperqz;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,6 +20,18 @@ public class info extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_info);
         FloatingActionButton bfab = findViewById(R.id.bfab);
+        EditText listb = findViewById(R.id.listb);
+        Button gihbt = findViewById(R.id.gihbt);
+        gihbt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Open github
+                Intent github = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/asciiblues/Paper_QZ"));
+                startActivity(github);
+            }
+        });
+        //LISTB as non-editable edittext
+        listb.setKeyListener(null);
         bfab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
